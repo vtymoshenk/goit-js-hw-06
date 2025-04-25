bc997361479ebcb1fc1c5e6e92cf74027740af3e
Viktoriia <vikatimoshenko126@gmail.com>
2025-04-25 18:04:47 +0200

    update all

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+class StringBuilder {
+  #value;
+  constructor(params) {
+    this.#value = params;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
